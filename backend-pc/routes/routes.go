9863237e69ba36438b9ec9ee14cf5db3124bdbf4
jupitters <sampaiobale@gmail.com>
2025-10-27 11:17:17 -0300

    route

---
+++ backend-pc/routes/routes.go
@@ -0,0 +1,10 @@
+package routes
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/jupitters/dear-livery/controller"
+)
+
+func OrderRoutes(router *mux.Router) {
+	router.HandleFunc("/order/", controller.GetOrders).Methods("GET")
+}
