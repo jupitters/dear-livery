446ce00e047a3f0f15dcd6bf1a7806d76dbb5026
jupitters <sampaiobale@gmail.com>
2025-10-27 23:36:44 -0300

    controller

--- backend-pc/controller/controller.go
+++ backend-pc/controller/controller.go
@@ -1,1 +1,16 @@
 package controller
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/jupitters/dear-livery/models"
+)
+
+func GetOrders(w http.ResponseWriter, r *http.Request) {
+	allOrders := models.GetAllOrders()
+	res, _ := json.Marshal(allOrders)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
