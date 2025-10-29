90e1cbf4d71be951b48434617d2b0e3f763400b1
jupitters <sampaiobale@gmail.com>
2025-10-29 14:53:35 -0300

    starting card

--- frontend/src/components/Orders.jsx
+++ frontend/src/components/Orders.jsx
@@ -3,12 +3,17 @@ import GlobalContext from "../GlobalContext";
 const Orders = () => {
     const { orders } = GlobalContext()
 
+
   return <section>
       {
         orders.map((order) => {
-          return (<div>
-            {order.address}
-          </div>) 
+          return <article key={order.id} className="single-order">
+            <img src="#" alt="" style={{width: '200px'}} className="img"/>
+            <footer>
+              <h5>Pedido nº: {order.id}</h5>
+            </footer>
+            
+          </article>
         })
       }
     </section>
