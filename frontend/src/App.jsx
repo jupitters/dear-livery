b0cb0ef6c87928a2fb019094382941a9d12f6b34
jupitters <sampaiobale@gmail.com>
2025-10-29 16:20:51 -0300

    image

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css"
 import Orders from "./components/Orders"
+import DeliveryBox from "./assets/delivery_box.jpg"
 
 function App() {
   return (
     <main>
+      <img src={DeliveryBox} />
       <Orders />
     </main>
   )
