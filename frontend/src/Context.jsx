b80898e8279d94499387f57353ca7e390071aaae
jupitters <sampaiobale@gmail.com>
2025-10-29 00:04:00 -0300

    axios

--- frontend/src/Context.jsx
+++ frontend/src/Context.jsx
@@ -2,14 +2,17 @@ import { createContext, useEffect } from 'react'
 
 const AppContext = createContext()
 
+// const ordersUrl = "http://localhost:9191/api/v1/orders/all"
+const randomUserUrl = 'https://randomuser.me/api/'
+
 const AppProvider = ({ children }) => {
 
     useEffect(()=> {
         const fetchData = async () => {
             try {
-                const response = await fetch('http://localhost:9191/api/v1/orders/all')
+                const response = await fetch(randomUserUrl)
                 const data = await response.json()
-                console.log(data)
+                console.log(data.results)
             } catch (error) {
                 console.log(error)
             }
