7806cdd19e3949b48693eea6745d6d64bee38490
jupitters <sampaiobale@gmail.com>
2025-10-28 23:58:13 -0300

    fetch data func

--- frontend/src/Context.jsx
+++ frontend/src/Context.jsx
@@ -1,8 +1,23 @@
-import { createContext } from 'react'
+import { createContext, useEffect } from 'react'
 
 const AppContext = createContext()
 
 const AppProvider = ({ children }) => {
+
+    useEffect(()=> {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('http://localhost:9191/api/v1/orders/all')
+                const data = await response.json()
+                console.log(data)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
+        fetchData()
+    }, [])
+
     return <AppContext.Provider value='Hello'>
         { children }
     </AppContext.Provider>
